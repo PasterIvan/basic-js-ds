aba48862789a5a2a93ce2c46554f2063262801bb
PasterIvan <paster-ivan@yandex.by>
2023-04-12 00:53:30 +0300

    init: init task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,31 +1,105 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
 class BinarySearchTree {
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (Node.data){
+      return Node;
+    }
+    return null
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if (Node.data === undefined) {
+      Node.data = new Node(data).data;
+      Node.left = new Node(data).left;
+      Node.right = new Node(data).right;
+    } else {
+      let current = Node
+      let prev = Node
+
+      const fun = () => {
+        if (data > current.data && current.right === null) {
+          current.right = new Node(data)
+        }
+        if (data < current.data && current.left === null) {
+          current.left = new Node(data)
+        }
+
+        if (data > current.data) {
+          if (data < current.right){
+            current = prev.right
+            prev.right = new Node(data)
+            prev.right.right = current
+          }
+          prev = current
+          current = current.right
+          fun()
+        }
+
+        if (data < current.data) {
+          if (data > current.left){
+            current = prev.left
+            prev.left = new Node(data)
+            prev.left.left = current
+          }
+          current = current.left
+          prev = current
+          fun()
+        }
+      }
+      fun()
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let current = false
+
+    const fun = () => {
+      if (data === current.data) {
+        return true
+      }
+      if (data > current.data) {
+        current = current.right
+        fun()
+      }
+      if (data < current.data) {
+        current = current.left
+        fun()
+      }
+      return false
+    }
+    if (current.data){
+      return fun();
+    }
+    return current
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+
+    let current = new Node()
+    const fun = () => {
+      if (data === current.data) {
+        return current
+      }
+      if (data > current.data) {
+        current = current.right
+        fun()
+      }
+      if (data < current.data) {
+        current = current.left
+        fun()
+      }
+      return current
+    }
+    fun()
   }
 
   remove(/* data */) {
